03f2626856b13ee97815cda214d82241d86f8edc
Ronny Wang <ronnywang@gmail.com>
2012-07-29 01:21:48 +0800

    add init.inc.php

---
+++ webdata/init.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+include(__DIR__ . '/stdlibs/pixframework/Pix/Loader.php');
+set_include_path(__DIR__ . '/stdlibs/pixframework/'
+    . PATH_SEPARATOR . __DIR__ . '/models'
+);
+
+Pix_Loader::registerAutoLoad();
+
+if (file_exists('config.php')) {
+    include('config.php');
+}
+$link = new mysqli;
+$link->connect(getenv('MYSQL_HOST'), getenv('MYSQL_USER'), getenv('MYSQL_PASS'));
+$link->select_db(getenv('MYSQL_DATABASE'));
+Pix_Table::setDefaultDb(new Pix_Table_Db_Adapter_Mysqli($link));
