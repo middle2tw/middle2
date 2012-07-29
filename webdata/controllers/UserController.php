b60f35d360f57ed68546b738ba2dbb104ddc25e9
Ronny Wang <ronnywang@gmail.com>
2012-07-29 16:16:26 +0800

    add ssh key managenet

--- webdata/controllers/UserController.php
+++ webdata/controllers/UserController.php
@@ -4,12 +4,47 @@ class UserController extends Pix_Controller
 {
     public function init()
     {
-        if (!$this->guest = Hisoku::getLoginUser()) {
+        if (!$this->user = Hisoku::getLoginUser()) {
             return $this->rediect('/');
         }
+        $this->view->user = $this->user;
     }
 
     public function indexAction()
     {
+    }
+
+    public function deletekeyAction()
+    {
+        if (Hisoku::getStoken() != $_POST['sToken']) {
+            // TODO: error
+            return $this->redirect('/');
+        }
+
+        list(, /*user*/, /*deletekey*/, $id) = explode('/', $this->getURI());
+        if (!$userkey = $this->user->keys->search(array('id' => $id))->first()) {
+            // TODO: error
+            return $this->redirect('/');
+        }
+
+        $userkey->delete();
+        return $this->redirect('/');
+    }
+
+    public function addkeyAction()
+    {
+        if (Hisoku::getStoken() != $_POST['sToken']) {
+            // TODO: error
+            return $this->redirect('/');
+        }
+
+        try {
+            $this->user->addKey($_POST['key']);
+        } catch (InvalidException $e) {
+            // TODO: error
+        } catch (Pix_Table_DuplicateException $e) {
+            // TODO: error
+        }
+        return $this->redirect('/');
     }
 }
