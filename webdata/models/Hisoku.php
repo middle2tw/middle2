2e22f88295f470f2eeef9aa538097ef47a2d51d3
Ronny Wang <ronnywang@gmail.com>
2012-07-29 14:17:46 +0800

    add Pix_Session support

--- webdata/models/Hisoku.php
+++ webdata/models/Hisoku.php
@@ -10,6 +10,9 @@ class Hisoku
 
     public function getLoginUser()
     {
+        if ($u = User::find(Pix_Session::get('user'))) {
+            return $u;
+        }
         return false;
     }
 }
--- webroot/index.php
+++ webroot/index.php
@@ -2,4 +2,5 @@
 
 include(__DIR__ . '/../webdata/init.inc.php');
 
+Pix_Session::setCore('cookie', array('secret' => getenv('SESSION_SECRET')));
 Pix_Controller::dispatch(__DIR__ . '/../webdata/');
