86441ab44a9a7f3ee1763f2c091de99a118f4a8d
Ronny Wang <ronnywang@gmail.com>
2012-07-29 14:18:01 +0800

    remove unused function

--- webdata/models/Hisoku.php
+++ webdata/models/Hisoku.php
@@ -2,12 +2,6 @@
 
 class Hisoku
 {
-    public function getView()
-    {
-        $view = new Pix_Partial(__DIR__ . '/../views');
-        return $view;
-    }
-
     public function getLoginUser()
     {
         if ($u = User::find(Pix_Session::get('user'))) {
