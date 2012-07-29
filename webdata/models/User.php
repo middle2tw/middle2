ca16c2645ffd2a40bac8d89325753a5a0675a14f
Ronny Wang <ronnywang@gmail.com>
2012-07-29 16:12:43 +0800

    add User->addKey

--- webdata/models/User.php
+++ webdata/models/User.php
@@ -2,6 +2,40 @@
 
 class UserRow extends Pix_Table_Row
 {
+    /**
+     * addKey
+     *
+     * @param string $keybody
+     * @access public
+     * @throw InvalidArgumentException
+     * @throw Pix_Table_DuplicateException
+     * @return UserKeyRow
+     */
+    public function addKey($keybody)
+    {
+        $keybody = trim($keybody);
+        $terms = explode(' ', $keybody);
+        if (3 !== count($terms)) {
+            throw new InvalidArgumentException('invalid key');
+        }
+        list($type, $body, $user) = $terms;
+        if (!in_array($type, array('ssh-rsa', 'ssh-dsa'))) {
+            throw new InvalidArgumentException('invalid ssh type');
+        }
+
+        if (preg_match('#[^a-zA-Z0-9/+=]#', $body)) {
+            throw new InvalidArgumentException('invalid ssh key');
+        }
+
+        $key = $this->keys->insert(array(
+            'key_fingerprint' => md5(base64_decode($body)),
+            'key_body' => $keybody,
+        ));
+
+        return $key;
+
+    }
+
     public function verifyPassword($password)
     {
         return $this->hashPassword($password) == $this->password;
@@ -40,5 +74,7 @@ class User extends Pix_Table
         $this->_columns['password'] = array('type' => 'char', 'size' => 64);
 
         $this->_indexes['name'] = array('type' => 'unique', 'columns' => array('name'));
+
+        $this->_relations['keys'] = array('rel' => 'has_many', 'type' => 'UserKey', 'foreign_key' => 'user_id', 'delete' => true);
     }
 }
