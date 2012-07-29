56219ac6425b7e06dfe7e45cd3e60ba6abc3c2df
Ronny Wang <ronnywang@gmail.com>
2012-07-29 15:13:29 +0800

    add key list

---
+++ webdata/models/UserKey.php
@@ -0,0 +1,19 @@
+<?php
+
+class UserKey extends Pix_Table
+{
+    public function init()
+    {
+        $this->_name = 'user_key';
+        $this->_primary = array('id');
+
+        $this->_columns['id'] = array('type' => 'int', 'auto_increment' => true);
+        $this->_columns['user_id'] = array('type' => 'int');
+        // ex: 23:47:08:1d:57:09:1b:f5:df:fe:96:52:0d:1d:57:5d
+        $this->_columns['key_fingerprint'] = array('type' => 'char', 'size' => 32);
+        $this->_columns['key_body'] = array('type' => 'text');
+
+        $this->_indexes['userid_id'] = array('type' => 'unique', 'columns' => array('user_id', 'id'));
+        $this->_indexes['key_fingerprint'] = array('type' => 'unique', 'columns' => array('key_fingerprint'));
+    }
+}
