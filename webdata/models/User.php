89d3f412b5876d10bbcdbfa49ee0bd838bebc6c2
Ronny Wang <ronnywang@gmail.com>
2012-07-29 14:29:12 +0800

    add password function

--- webdata/models/User.php
+++ webdata/models/User.php
@@ -2,6 +2,27 @@
 
 class UserRow extends Pix_Table_Row
 {
+    public function verifyPassword($password)
+    {
+        return $this->hashPassword($psasword) == $this->password;
+    }
+
+    public function setPassword($password)
+    {
+        $this->password_type = 1;
+        $this->password = $this->hashPassword($password);
+        $this->save();
+    }
+
+    public function hashPassword($password)
+    {
+        switch ($this->password_type) {
+        case 1:
+            return hash_hmac('sha256', $password, $this->name, true);
+        default:
+            throw new Excpetion('unknown password_type');
+        }
+    }
 }
 
 class User extends Pix_Table
@@ -14,6 +35,7 @@ class User extends Pix_Table
 
         $this->_columns['id'] = array('type' => 'int', 'auto_increment' => true);
         $this->_columns['name'] = array('type' => 'varchar', 'size' => 32);
+        // password_type: 1-hmac_sha256
         $this->_columns['password_type'] = array('type' => 'tinyint');
         $this->_columns['password'] = array('type' => 'char', 'size' => 64);
 
