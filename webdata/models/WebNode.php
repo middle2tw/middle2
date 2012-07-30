896bd2c12f4684f88594b44967d3cb8847dccc14
Ronny Wang <ronnywang@gmail.com>
2012-07-30 23:04:30 +0800

    port change to int

---
+++ webdata/models/WebNode.php
@@ -0,0 +1,19 @@
+<?php
+
+class WebNode extends Pix_Table
+{
+    public function init()
+    {
+        $this->_name = 'webnode';
+        $this->_primary = array('ip', 'port');
+
+        $this->_columns['ip'] = array('type' => 'int', 'unsigned' => true);
+        $this->_columns['port'] = array('type' => 'int');
+        $this->_columns['project_id'] = array('type' => 'int');
+        $this->_columns['created_at'] = array('type' => 'int');
+        $this->_columns['start_at'] = array('type' => 'int');
+        $this->_columns['access_at'] = array('type' => 'int');
+
+        $this->_indexes['project_id'] = array('project_id');
+    }
+}
