a7d0a2674eea35c068d4618fb12abd8033f43b42
Ronny Wang <ronnywang@gmail.com>
2012-08-04 17:47:01 +0800

    setCore => setAdapter

--- webroot/index.php
+++ webroot/index.php
@@ -2,6 +2,6 @@
 
 include(__DIR__ . '/../webdata/init.inc.php');
 
-Pix_Session::setCore('cookie', array('secret' => getenv('SESSION_SECRET')));
+Pix_Session::setAdapter('cookie', array('secret' => getenv('SESSION_SECRET')));
 Pix_Controller::addCommonHelpers();
 Pix_Controller::dispatch(__DIR__ . '/../webdata/');
