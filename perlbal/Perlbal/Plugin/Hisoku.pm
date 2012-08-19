f45398eb05022451b3e5eb124e41b2d171715395
Ronny Wang <ronnywang@gmail.com>
2012-08-19 22:01:40 +0800

    use http request get nodes info

--- perlbal/Perlbal/Plugin/Hisoku.pm
+++ perlbal/Perlbal/Plugin/Hisoku.pm
@@ -2,6 +2,8 @@ package Perlbal::Plugin::Hisoku;
 
 use strict;
 use warnings;
+use WWW::Curl::Easy;
+use JSON -support_by_pp;
 no  warnings qw(deprecated);
 
 # when "LOAD" directive loads us up
@@ -25,14 +27,23 @@ sub register {
             my Perlbal::HTTPHeaders $hds = $client->{req_headers};
             return 0 unless $hds;
 
-            my $vhost = $hds->header("Host");
+            my ($vhost_ip, $vhost_port) = split(':', $hds->header("Host"));
 
-            my $ipport = `sudo -u ec2-user /srv/code/hisoku/scripts/get-nodes-by-domain $vhost`;
-            $ipport =~ s/\s+$//m;
-            my ($ip, $port) = split(':', $ipport);
+            my $curl = WWW::Curl::Easy->new;
+            $curl->setopt(CURLOPT_HEADER,1);
+            $curl->setopt(CURLOPT_URL, 'http://hisoku.ronny.tw/api/getnodes?domain=' . $vhost_ip . '&port=' . $vhost_port);
+
+            my $response_body;
+            open (my $fileb, ">", \$response_body);
+            $curl->setopt(CURLOPT_WRITEDATA, $fileb);
+            my $retcode = $curl->perform;
+            my $json;
+            $json = from_json($response_body, {utf8 => 1});
+            my $ip = $json->{'nodes'}[0][0];
+            my $port = $json->{'nodes'}[0][1];
 
             if (my $be = Perlbal::BackendHTTP->new($svc, $ip, $port, { pool => $svc->{pool} })) {
-                    $svc->add_pending_connect($be);
+                $svc->add_pending_connect($be);
             }
     });
 
