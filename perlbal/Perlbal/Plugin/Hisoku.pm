ada585344a634e2aebbfe7d35931d2646cd75193
Ronny Wang <ronnywang@gmail.com>
2012-08-19 14:26:28 +0000

    change vhost_ip to vhost_domain, hisoku.ronny.tw => 127.0.0.1:9999

--- perlbal/Perlbal/Plugin/Hisoku.pm
+++ perlbal/Perlbal/Plugin/Hisoku.pm
@@ -27,17 +27,18 @@ sub register {
             my Perlbal::HTTPHeaders $hds = $client->{req_headers};
             return 0 unless $hds;
 
-            my ($vhost_ip, $vhost_port) = split(':', $hds->header("Host"));
+            my ($vhost_domain, $vhost_port) = split(':', $hds->header("Host"));
             $vhost_port = 80 unless defined $vhost_port;
 
             my $curl = WWW::Curl::Easy->new;
-            $curl->setopt(CURLOPT_HEADER,1);
-            $curl->setopt(CURLOPT_URL, 'http://hisoku.ronny.tw/api/getnodes?domain=' . $vhost_ip . '&port=' . $vhost_port);
+            $curl->setopt(CURLOPT_HEADER,0);
+            $curl->setopt(CURLOPT_URL, 'http://127.0.0.1:9999/api/getnodes?domain=' . $vhost_domain. '&port=' . $vhost_port);
 
             my $response_body;
             open (my $fileb, ">", \$response_body);
             $curl->setopt(CURLOPT_WRITEDATA, $fileb);
             my $retcode = $curl->perform;
+
             my $json;
             $json = from_json($response_body, {utf8 => 1});
             my $ip = $json->{'nodes'}[0][0];
