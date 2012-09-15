d365ff4b7d5a30c596c5e8ba497dbc1d04b29fab
Ronny Wang <ronnywang@gmail.com>
2012-09-15 15:58:26 +0800

    add custom_domain crud

--- webdata/controllers/ProjectController.php
+++ webdata/controllers/ProjectController.php
@@ -27,4 +27,52 @@ class ProjectController extends Pix_Controller
         $this->view->project = $project;
 
     }
+
+    public function deletedomainAction()
+    {
+        if (Hisoku::getStoken() != $_POST['sToken']) {
+            // TODO: error
+            return $this->redirect('/');
+        }
+
+        list(, /*project*/, /*adddomain*/, $name) = explode('/', $this->getURI());
+        if (!$project = Project::find_by_name($name)) {
+            // TODO: 404
+            return $this->redirect('/');
+        }
+
+        if (!$domain = $project->custom_domains->search(array('domain' => $_GET['domain']))->first()) {
+            // TODO: 404
+            return $this->redirect('/');
+        }
+
+        $domain->delete();
+        return $this->redirect('/project/detail/' . $project->name);
+    }
+
+    public function adddomainAction()
+    {
+        if (Hisoku::getStoken() != $_POST['sToken']) {
+            // TODO: error
+            return $this->redirect('/');
+        }
+
+        list(, /*project*/, /*adddomain*/, $name) = explode('/', $this->getURI());
+        if (!$project = Project::find_by_name($name)) {
+            // TODO: 404
+            return $this->redirect('/');
+        }
+
+        // from http://myregexp.com/examples.html
+        if (!preg_match('#^([a-zA-Z0-9]([a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}$#', $_POST['domain'])) {
+            // TODO: 404
+            return $this->redirect('/');
+        }
+
+        $project->custom_domains->insert(array(
+            'domain' => strval($_POST['domain']),
+        ));
+
+        return $this->redirect('/project/detail/' . $project->name);
+    }
 }
