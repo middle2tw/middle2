ccd4fc9d468cc1e52dda787cf8c97d3f3674327e
Ronny Wang <ronnywang@gmail.com>
2012-09-17 00:34:49 +0800

    add Hisoku::uniqid($length)

--- webdata/models/Hisoku.php
+++ webdata/models/Hisoku.php
@@ -19,4 +19,15 @@ class Hisoku
 
         return $sToken;
     }
+
+    public static function uniqid($length)
+    {
+        $set = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        $ret = '';
+        for ($i = 0; $i < $length; $i ++) {
+            $ret .= $set[rand(0, strlen($set) - 1)];
+        }
+        return $ret;
+    }
+
 }
