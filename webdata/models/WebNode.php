c9849d553bd4bee7ad4092446ffeb7f3b2d803e4
Ronny Wang <ronnywang@gmail.com>
2012-09-22 01:31:17 +0800

    add status column

--- webdata/models/WebNode.php
+++ webdata/models/WebNode.php
@@ -11,6 +11,9 @@ class WebNode extends Pix_Table
         $this->_columns['port'] = array('type' => 'int');
         $this->_columns['project_id'] = array('type' => 'int');
         $this->_columns['commit'] = array('type' => 'char', 'size' => 32);
+        // status: 0-unused, 1-processing
+        //         10-webnode, 11-cronnode
+        $this->_columns['status'] = array('type' => 'tinyint');
         $this->_columns['created_at'] = array('type' => 'int');
         $this->_columns['start_at'] = array('type' => 'int');
         $this->_columns['access_at'] = array('type' => 'int');
