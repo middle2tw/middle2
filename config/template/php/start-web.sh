2439bc889777ba5c316a92c89036d0443d1d7d4b
Ronny Wang <ronnywang@gmail.com>
2012-09-25 01:39:56 +0800

    remove -v

--- config/template/php/start-web.sh
+++ config/template/php/start-web.sh
@@ -5,7 +5,7 @@ log_category=$1
 echo $log_category > /srv/log_category
 
 # build /srv/env.conf
-env -v | awk -F '=' '{print "env[" $1 "]=" $2}' > /srv/env.conf
+env | awk -F '=' '{print "env[" $1 "]=" $2}' > /srv/env.conf
 
 # start php-fpm
 /usr/sbin/php-fpm
