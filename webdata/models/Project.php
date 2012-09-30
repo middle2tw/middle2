68fd3e706a248d7af5ef90673f8db3729878b8cd
Ronny Wang <ronnywang@gmail.com>
2012-09-30 16:52:44 +0800

    add Project has_many cronjobs relation

--- webdata/models/Project.php
+++ webdata/models/Project.php
@@ -39,6 +39,7 @@ class Project extends Pix_Table
         $this->_relations['custom_domains'] = array('rel' => 'has_many', 'type' => 'CustomDomain', 'foreign_key' => 'project_id');
         $this->_relations['variables'] = array('rel' => 'has_many', 'type' => 'ProjectVariable', 'foreign_key' => 'project_id');
         $this->_relations['webnodes'] = array('rel' => 'has_many', 'type' => 'WebNode', 'foreign_key' => 'project_id');
+        $this->_relations['cronjobs'] = array('rel' => 'has_many', 'type' => 'CronJob', 'foreign_key' => 'project_id');
 
         $this->_hooks['eavs'] = array('get' => 'getEAVs');
 
