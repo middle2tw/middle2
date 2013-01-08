548354726c2f1e4ad2ade609c62b298eae0a4812
Ronny Wang <ronnywang@gmail.com>
2013-01-08 10:20:14 +0800

    增加 admin 後台新增加 node 的動作

--- webdata/controllers/AdminController.php
+++ webdata/controllers/AdminController.php
@@ -23,4 +23,33 @@ class AdminController extends Pix_Controller
     public function nodeserversAction()
     {
     }
+
+    public function nodeserveraddportAction()
+    {
+        list(, /*admin*/, /*nodeserveraddport*/, $ip, $port) = explode('/', $this->getURI());
+        if ($_POST['sToken'] != Hisoku::getStoken()) {
+            return $this->alert('wrong stoken', '/admin');
+        }
+
+        if (!filter_var($ip, FILTER_VALIDATE_IP)) {
+            return $this->alert('wrong ip', '/admin');
+        }
+
+        $port = intval($port);
+        if ($port <= 0 or $port > 100) {
+            return $this->alert('port must in 1 ~ 100', '/admin');
+        }
+
+        if (WebNode::find(array(ip2long($ip), 20000 + $port))) {
+            return $this->alert('port ' . (20000 + $port) . ' is existed', '/admin');
+        }
+
+        try {
+            WebNode::initNode($ip, $port);
+        } catch (Exception $e) {
+            return $this->alert($e->getMessage(), '/admin');
+        }
+
+        return $this->alert('done', '/admin');
+    }
 }
