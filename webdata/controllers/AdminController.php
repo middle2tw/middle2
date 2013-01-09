6342ecaacb48bb37f2a7c3d54ac8b0cdbef835e4
Ronny Wang <ronnywang@gmail.com>
2013-01-09 14:55:16 +0800

    加上 stop 功能

--- webdata/controllers/AdminController.php
+++ webdata/controllers/AdminController.php
@@ -106,4 +106,33 @@ class AdminController extends Pix_Controller
 
         return $this->alert('done', '/admin');
     }
+
+    public function nodeserverstopportAction()
+    {
+        list(, /*admin*/, /*nodeserverstopport*/, $ip, $port) = explode('/', $this->getURI());
+
+        if ($_POST['sToken'] != Hisoku::getStoken()) {
+            return $this->alert('wrong stoken', '/admin');
+        }
+
+        if (!filter_var($ip, FILTER_VALIDATE_IP)) {
+            return $this->alert('wrong ip', '/admin');
+        }
+
+        $port = intval($port);
+        if ($port <= 0 or $port > 100) {
+            return $this->alert('port must in 1 ~ 100', '/admin');
+        }
+
+        if (!$node = WebNode::find(array(ip2long($ip), 20000 + $port))) {
+            return $this->alert('port ' . (20000 + $port) . ' is not found', '/admin');
+        }
+        $node->update(array(
+            'project_id' => 0,
+            'commit' => '',
+            'status' => WebNode::STATUS_STOP,
+        ));
+
+        return $this->alert('done', '/admin');
+    }
 }
