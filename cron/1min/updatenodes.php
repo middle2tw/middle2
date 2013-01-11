67e9a2ad6512e09eab3f3fc5eb1cdf176a078f36
Ronny Wang <ronnywang@gmail.com>
2013-01-11 11:46:27 +0800

    加上 lock 機制

--- cron/1min/updatenodes.php
+++ cron/1min/updatenodes.php
@@ -1,4 +1,19 @@
 #!/usr/bin/env php
 <?php
+
+define('LOCK_FILE', '/tmp/hisoku-updatenodes');
+$fp = fopen(LOCK_FILE, 'w+');
+if (!flock($fp, LOCK_EX | LOCK_NB)) {
+    throw new Exception(LOCK_FILE . ' is locking');
+}
+
+ftruncate($fp, 0);      // truncate file
+fwrite($fp, getmypid());
+fflush($fp);            // flush output before releasing the lock
+
 include(__DIR__ . '/../../webdata/init.inc.php');
 WebNode::updateNodeInfo();
+
+flock($fp, LOCK_UN);    // release the lock
+fclose($fp);
+unlink(LOCK_FILE);
