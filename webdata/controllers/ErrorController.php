1d104f057d1aabd99b7d50f2fc48ac55cce68720
Ronny Wang <ronnywang@gmail.com>
2013-01-14 11:36:14 +0800

    錯誤訊息不要直接印出來，改用 trigger_error

--- webdata/controllers/ErrorController.php
+++ webdata/controllers/ErrorController.php
@@ -4,8 +4,13 @@ class ErrorController extends Pix_Controller
 {
     public function errorAction()
     {
-        echo var_dump($this->view->exception);
-        exit;
+        if ($this->view->exception instanceof Pix_Controller_Dispatcher_Exception) {
+            echo '404';
+            return $this->noview();
+        } else {
+            trigger_error(strval($this->view->exception), E_USER_WARNING);
+            return $this->noview();
+        }
     }
 
     public function notfoundAction()
