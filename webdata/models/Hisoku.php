23369b5a902176b1cdae9ea0715c75ce194c818e
Ronny Wang <ronnywang@gmail.com>
2013-01-23 10:14:50 +0800

    add Hisoku::alert(title, body)

--- webdata/models/Hisoku.php
+++ webdata/models/Hisoku.php
@@ -30,4 +30,23 @@ class Hisoku
         return $ret;
     }
 
+    public static function alert($title, $body)
+    {
+        if (!class_exists('AmazonSNS')) {
+            define('AWS_DISABLE_CONFIG_AUTO_DISCOVERY', true);
+            include(__DIR__ . '/../stdlibs/sdk-1.6.0/sdk.class.php');
+            if (!getenv('SNS_KEY') or !getenv('SNS_SECRET')) {
+                throw new Exception('env SNS_KEY & SNS_SECRET not found');
+            }
+            CFCredentials::set(array(
+                'development' => array(
+                    'key' => getenv('SNS_KEY'),
+                    'secret' => getenv('SNS_SECRET'),
+                ),
+            ));
+        }
+        $sns = new AmazonSNS();
+        $sns->set_region(AmazonSNS::REGION_TOKYO);
+        $sns->publish('arn:aws:sns:ap-northeast-1:391093844476:Hisoku-Health', $body, array('Subject' => $title));
+    }
 }
