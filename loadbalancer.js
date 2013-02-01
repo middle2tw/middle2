c295d81a4858ac1a77e50b9a03185404052a1e66
Ronny Wang <ronnywang@gmail.com>
2013-02-01 15:05:31 +0800

    改成在 loadbalancer 寫入 log

--- loadbalancer.js
+++ loadbalancer.js
@@ -1,8 +1,38 @@
 var http = require('http');
+var Scribe = require('scribe').Scribe;
+scribe = new Scribe("scribe.hisoku.ronny.tw", 1463, {"autoReconnect": true});
+scribe.open();
 
 if (process.argv.length < 4) {
     throw "Usage: node loadbalancer.js [server-ip] [server-port]";
 }
+
+var formatdate = function(){
+    var d = new Date;
+    // 2012-12-32 12:32:33 -480
+    return d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds() + ' ' + d.getTimezoneOffset();
+};
+
+var pad = function(n, length){
+    var ret = '' + n;
+    for (; ret.length < length; ) {
+        ret = '0' + ret;
+    }
+    return ret;
+};
+
+var apachedate = function(){
+    // [31/Jan/2013:16:57:47 -0500]
+    var d = new Date;
+    var month_locale = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    var zone = '';
+    if (d.getTimezoneOffset() <= 0) {
+        zone = '+' + pad(-100 * d.getTimezoneOffset() / 60, 4);
+    } else {
+        zone = '-' + pad(100 * d.getTimezoneOffset() / 60, 4);
+    }
+    return '[' + pad(d.getDate(), 2) + '/' + month_locale[d.getMonth()] + '/' + d.getFullYear() + ':' + pad(d.getHours(), 2) + ':' + pad(d.getMinutes(), 2) + ':' + pad(d.getSeconds(), 2) + ' ' + zone + ']';
+};
 
 var hisoku = {};
 hisoku.cache = {};
@@ -35,11 +65,10 @@ hisoku.getBackendHost = function(host, port, callback){
             if (json.error) {
                 return callback({success: false});
             }
-            return callback({success: true, host: json.nodes[0][0], port: json.nodes[0][1]});
+            return callback({success: true, host: json.nodes[0][0], port: json.nodes[0][1], project: json.project});
         });
         selector_response.on('close', function(){
-            // TODO : error
-            console.log(JSON.parse(data));
+            scribe.send('lb-error', formatdate() + ' selector_response_close ' + JSON.stringify(JSON.parse(data)));
         });
     }).end();
 };
@@ -73,7 +102,6 @@ main_request.on('request', function(main_request, main_response){
         main_request.headers['x-forwarded-for'] = main_request.socket.remoteAddress;
         main_request.headers['x-forwarded-port'] = main_request.socket.address().port;
     }
-    console.log(host + ' ' + main_request.url);
 
     main_request.on('data', function(chunk){
         main_request_data(chunk);
@@ -86,6 +114,23 @@ main_request.on('request', function(main_request, main_response){
 
     hisoku.getBackendHost(host, port, function(options){
         if (!options.success) {
+            var referer = main_request.headers['referer'];
+            if (typeof(referer) != 'string') {
+                referer = '-';
+            }
+            var useragent = main_request.headers['user-agent'];
+            if (typeof(useragent) != 'string') {
+                useragent = '-';
+            }
+            var log = (host
+                + ' ' + main_request.headers['x-forwarded-for']
+                + ' - - ' + apachedate()
+                + ' "' + main_request.method.toUpperCase() + ' ' + main_request.url + ' HTTP/' + main_request.httpVersion + '"'
+                + ' 404 0'
+                + ' "' + referer + '"'
+                + ' "' + useragent + '"'
+            ); 
+            scribe.send('lb-notfound', log);
             main_response.writeHead(302, {Location: 'http://hisoku.ronny.tw/error/notfound'});
             main_response.end();
             return;
@@ -100,7 +145,8 @@ main_request.on('request', function(main_request, main_response){
 
         var backend_host = options.host;
         var backend_port = options.port;
-        console.log(host + ' ' + backend_host + ' ' + backend_port);
+        var return_length = 0;
+        //console.log(host + ' ' + backend_host + ' ' + backend_port);
 
         var backend_request = http.request({
             hostname: backend_host,
@@ -114,20 +160,38 @@ main_request.on('request', function(main_request, main_response){
             main_response.writeHead(backend_response.statusCode, backend_response.headers);
             backend_response.on('data', function(chunk){
                 main_response.write(chunk);
+                return_length += chunk.length;
             });
 
             backend_response.on('end', function(){
+                if ('string' == typeof(options.project)) {
+                    var referer = main_request.headers['referer'];
+                    if (typeof(referer) != 'string') {
+                        referer = '-';
+                    }
+                    var useragent = main_request.headers['user-agent'];
+                    if (typeof(useragent) != 'string') {
+                        useragent = '-';
+                    }
+                    var log = (host
+                        + ' ' + main_request.headers['x-forwarded-for']
+                        + ' - - ' + apachedate()
+                        + ' "' + main_request.method.toUpperCase() + ' ' + main_request.url + ' HTTP/' + main_request.httpVersion + '"'
+                        + ' ' + backend_response.statusCode + ' ' + return_length 
+                        + ' "' + referer + '"'
+                        + ' "' + useragent + '"'); 
+                    scribe.send('app-' + options.project, log);
+                }
                 main_response.end();
             });
 
             backend_response.on('close', function(){
-                // TODO: log error
+                scribe.send('lb-error', formatdate() + ' backend_response_close');
             });
         });
 
         backend_request.on('error', function(e){
-            // TODO: log error
-            console.log('error: ' + e + '(host: ' + host + ', backend_host: ' + backend_host + ', backend_port: ' + backend_port + ')');
+            scribe.send('lb-error', formatdate() + ' backend_request_error' + ' ' + host + ' ' + backend_host + ' ' + backend_port);
             main_response.end();
         });
 
