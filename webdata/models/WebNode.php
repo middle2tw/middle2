a087fef8c6ab5ca81e37c5f761c08bdeffef4254
Ronny Wang <ronnywang@gmail.com>
2013-02-06 11:43:16 +0800

    start_at and access_at 都要超過五分鐘才砍掉

--- webdata/models/WebNode.php
+++ webdata/models/WebNode.php
@@ -287,7 +287,7 @@ class WebNode extends Pix_Table
             }
 
             // 如果 processing node 太久也要踢掉
-            if (in_array($node->status, array(WebNode::STATUS_CRONPROCESSING, WebNode::STATUS_WEBPROCESSING)) and (time() - $node->getAccessAt()) > 300) {
+            if (in_array($node->status, array(WebNode::STATUS_CRONPROCESSING, WebNode::STATUS_WEBPROCESSING)) and (time() - $node->getAccessAt()) > 300 and (time() - $node->start_at) > 300) {
                 $processes = $node->getNodeProcesses();
                 if (0 == count($processes)) {
                     trigger_error("{$node->ip}:{$node->port}(status=processing) had no alive process, release it", E_USER_WARNING);
