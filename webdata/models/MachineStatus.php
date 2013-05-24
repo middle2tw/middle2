90fc4a9fb1f9bc1dbad58644204fac519a1a7f0e
Ronny Wang <ronnywang@gmail.com>
2013-05-24 17:50:27 +0800

    增加 MachineStatus 存機器狀況

---
+++ webdata/models/MachineStatus.php
@@ -0,0 +1,14 @@
+<?php
+
+class MachineStatus extends Pix_Table
+{
+    public function init()
+    {
+        $this->_name = 'machine_status';
+        $this->_primary = 'name';
+
+        $this->_columns['name'] = array('type' => 'varchar', 'size' => 64);
+        $this->_columns['status'] = array('type' => 'text');
+        $this->_columns['updated_at'] = array('type' => 'int');
+    }
+}
