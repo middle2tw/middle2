f5455badeba9b61912c67189146089c94dbe0063
Ronny Wang <ronnywang@gmail.com>
2013-05-24 17:52:08 +0800

    pk 改成包含updated_at

--- webdata/models/MachineStatus.php
+++ webdata/models/MachineStatus.php
@@ -5,7 +5,7 @@ class MachineStatus extends Pix_Table
     public function init()
     {
         $this->_name = 'machine_status';
-        $this->_primary = 'name';
+        $this->_primary = array('name', 'updated_at');
 
         $this->_columns['name'] = array('type' => 'varchar', 'size' => 64);
         $this->_columns['status'] = array('type' => 'text');
