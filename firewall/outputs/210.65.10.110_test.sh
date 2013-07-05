71e26b338f1171877f71a5ea06e7acd20ee42872
Ronny Wang <ronnywang@gmail.com>
2013-07-05 16:05:02 +0800

    add firewall config

---
+++ firewall/outputs/210.65.10.110_test.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+iptables -F
+iptables -X
+iptables -Z
+iptables -P INPUT DROP
+iptables -P OUTPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+# allow all from categories ALL, dev, git
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+# allow all from categories loadbalancer
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+# allow all from categories loadbalancer
+iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+# allow loadbalancer, node from categories scribe
+iptables -A INPUT -p tcp -s 210.65.11.137 --dport 1426 -j ACCEPT
+# allow loadbalancer from categories mainpage
+# allow loadbalancer, mainpage from categories private_memcache
+sleep 30
+iptables -F
+iptables -X
+iptables -Z
+iptables -P INPUT ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -P FORWARD ACCEPT
---
+++ firewall/outputs/210.65.11.197.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+iptables -F
+iptables -X
+iptables -Z
+iptables -P INPUT DROP
+iptables -P OUTPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+# allow dev from categories ALL
+iptables -A INPUT -p tcp -s 210.65.10.110 --dport 22 -j ACCEPT
+# allow loadbalancer, mainpage, node from categories mysql
+iptables -A INPUT -p tcp -s 210.65.10.110 --dport 3306 -j ACCEPT
+iptables -A INPUT -p tcp -s 210.65.11.137 --dport 3306 -j ACCEPT
+# allow all from categories pgsql
+iptables -A INPUT -p tcp --dport 5432 -j ACCEPT
+# allow node, mainpage from categories elastic_search
+iptables -A INPUT -p tcp -s 210.65.11.137 --dport 9200 -j ACCEPT
+iptables -A INPUT -p tcp -s 210.65.10.110 --dport 9200 -j ACCEPT
