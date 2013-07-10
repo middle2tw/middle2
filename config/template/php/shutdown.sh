a6d1b629f14e7a4d5fa45a55f607e30552ddf6bd
Ronny Wang <ronnywang@gmail.com>
2013-07-10 17:53:31 +0800

    移除 config/template, 已經都搬到 config/rpmsource 下面了

--- config/template/php/shutdown.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ -f "/var/run/php-fpm.pid" ]; then
-    kill `cat /var/run/php-fpm.pid`
-fi
-
-if [ -f "/usr/local/apache2/logs/httpd.pid" ]; then
-    kill `cat /usr/local/apache2/logs/httpd.pid`
-fi
