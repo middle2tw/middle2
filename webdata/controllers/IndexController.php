d3eb2e0c1ca733d186c11805800e37710ab6f341
Ronny Wang <ronnywang@gmail.com>
2013-07-15 09:26:24 +0800

    加上 log 登入記錄

--- webdata/controllers/IndexController.php
+++ webdata/controllers/IndexController.php
@@ -13,19 +13,24 @@ class IndexController extends Pix_Controller
     {
         $alert_message = "Invalid user name or password";
         if (!$u = User::find_by_name(strval($_POST['user']))) {
+            Logger::log(array(array('category' => 'login', 'message' => time() . " user-not-found {$_SERVER['REMOTE_ADDR']} user=" . urlencode($_POST['user']) . ",agent=" . urlencode($_SERVER['HTTP_USER_AGENT']))));
             return $this->alert($alert_message, '/');
         }
        
         if (!$u->verifyPassword($_POST['password'])) {
+            Logger::log(array(array('category' => 'login', 'message' => time() . " wrong-password {$_SERVER['REMOTE_ADDR']} user=" . urlencode($_POST['user']) . ",agent=" . urlencode($_SERVER['HTTP_USER_AGENT']))));
             return $this->alert($alert_message, '/');
         }
 
         Pix_Session::set('user', $u->id);
+        Logger::log(array(array('category' => 'login', 'message' => time() . " ok {$_SERVER['REMOTE_ADDR']} user=" . urlencode($_POST['user']) . ",agent=" . urlencode($_SERVER['HTTP_USER_AGENT']))));
         return $this->redirect('/');
     }
 
     public function logoutAction()
     {
+        $user = Hisoku::getLoginUser();
+        Logger::log(array(array('category' => 'login', 'message' => time() . " logout {$_SERVER['REMOTE_ADDR']} user=" . urlencode($user->name) . ",agent=" . urlencode($_SERVER['HTTP_USER_AGENT']))));
         Pix_Session::delete('user');
         return $this->redirect('/');
     }
