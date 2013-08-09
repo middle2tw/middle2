3aca077857d898c2b1ebb6861d5697b3bbe7dd1d
Ronny Wang <ronnywang@gmail.com>
2013-08-09 17:43:31 +0800

    add Addon_MySQLDBMember->saveProjectVariable()

--- webdata/models/Addon/MySQLDBMember.php
+++ webdata/models/Addon/MySQLDBMember.php
@@ -1,11 +1,31 @@
 <?php
 
+class Addon_MySQLDBMemberRow extends Pix_Table_Row
+{
+    public function saveProjectVariable()
+    {
+        try {
+            $this->project->variables->insert(array(
+                'key' => 'DATABASE_URL',
+                'value' => "mysql://{$this->username}:{$this->password}@{$this->addon->host}/{$this->addon->database}",
+            ));
+        } catch (Pix_Table_DuplicateException $e) {
+            $this->project->variables->search(array(
+                'key' => 'DATABASE_URL',
+            ))->update(array(
+                'value' => "mysql://{$this->username}:{$this->password}@{$this->addon->host}/{$this->addon->database}",
+            ));
+        }
+    }
+}
+
 class Addon_MySQLDBMember extends Pix_Table
 {
     public function init()
     {
         $this->_name = 'addon_mysqldb_member';
         $this->_primary = array('addon_id', 'project_id');
+        $this->_rowClass = 'Addon_MySQLDBMemberRow';
 
         $this->_columns['addon_id'] = array('type' => 'int');
         $this->_columns['project_id'] = array('type' => 'int');
