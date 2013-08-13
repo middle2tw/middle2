0287a4d7089add3acdc43af3543c47ecb29ca3f9
Ronny Wang <ronnywang@gmail.com>
2013-08-13 10:25:21 +0800

    移除之前新增的兩台 nodes server

--- firewall/outputs/210.65.11.197.sh
+++ firewall/outputs/210.65.11.197.sh
@@ -12,12 +12,8 @@ iptables -A INPUT -p tcp -s 210.65.10.110 --dport 22 -j ACCEPT
 # allow loadbalancer, mainpage, node from categories mysql
 iptables -A INPUT -p tcp -s 210.65.10.110 --dport 3306 -j ACCEPT
 iptables -A INPUT -p tcp -s 210.61.2.239 --dport 3306 -j ACCEPT
-iptables -A INPUT -p tcp -s 210.61.2.65 --dport 3306 -j ACCEPT
-iptables -A INPUT -p tcp -s 203.66.14.47 --dport 3306 -j ACCEPT
 # allow all from categories pgsql
 iptables -A INPUT -p tcp --dport 5432 -j ACCEPT
 # allow node, mainpage from categories elastic_search
 iptables -A INPUT -p tcp -s 210.61.2.239 --dport 9200 -j ACCEPT
-iptables -A INPUT -p tcp -s 210.61.2.65 --dport 9200 -j ACCEPT
-iptables -A INPUT -p tcp -s 203.66.14.47 --dport 9200 -j ACCEPT
 iptables -A INPUT -p tcp -s 210.65.10.110 --dport 9200 -j ACCEPT
