1f5364e110684200cced37b2068b414623e8f20a
Ronny Wang <ronnywang@gmail.com>
2013-08-16 13:53:44 +0800

    增加 MachineRow 並且加上 ->setGroups(), ->getGroups()

--- webdata/models/Machine.php
+++ webdata/models/Machine.php
@@ -1,11 +1,26 @@
 <?php
 
+class MachineRow extends Pix_Table_Row
+{
+    public function getGroups()
+    {
+        return explode(',', $this->groups);
+    }
+
+    public function setGroups($groups)
+    {
+        $this->groups = $groups;
+        $this->save();
+    }
+}
+
 class Machine extends Pix_Table
 {
     public function init()
     {
         $this->_name = 'machine';
         $this->_primary = 'machine_id';
+        $this->_rowClass = 'MachineRow';
 
         $this->_columns['machine_id'] = array('type' => 'int', 'auto_increment' => true);
         $this->_columns['ip'] = array('type' => 'int', 'unsigned' => true);
