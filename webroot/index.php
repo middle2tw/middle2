f59254b923e0b1378cd9c751d06d54c48b13fbe5
Ronny Wang <ronnywang@gmail.com>
2013-08-19 01:08:04 +0800

    加上 setproctitle ，印出卡死的網頁是哪一頁

--- webroot/index.php
+++ webroot/index.php
@@ -2,6 +2,12 @@
 
 include(__DIR__ . '/../webdata/init.inc.php');
 
+if (function_exists('setproctitle')) {
+    setproctitle('middle2: ' . $_SERVER['REQUEST_URI']);
+}
 Pix_Session::setAdapter('cookie', array('secret' => getenv('SESSION_SECRET'), 'cookie_key' => 'HISOKU_SESSION'));
 Pix_Controller::addCommonHelpers();
 Pix_Controller::dispatch(__DIR__ . '/../webdata/');
+if (function_exists('setproctitle')) {
+    setproctitle('php-fpm');
+}
