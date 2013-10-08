3d8d02f1eac13ba35369922a5f259688f1fed4c9
Ronny Wang <ronnywang@gmail.com>
2013-10-08 09:42:41 +0800

    remove 210.61.2.239

--- firewall/outputs/210.65.10.110_test.sh
+++ firewall/outputs/210.65.10.110_test.sh
@@ -17,27 +17,20 @@ iptables -A INPUT -p tcp --dport 80 -j ACCEPT
 iptables -A INPUT -p tcp --dport 443 -j ACCEPT
 # allow loadbalancer, node from categories scribe
 iptables -A INPUT -p tcp -s 210.65.11.197 --dport 1426 -j ACCEPT
-iptables -A INPUT -p tcp -s 210.61.2.239 --dport 1426 -j ACCEPT
 # allow loadbalancer from categories mainpage
 # allow loadbalancer, mainpage from categories private_memcache
 # allow node from categories nfs
 iptables -A INPUT -p tcp -s 210.65.11.197 --dport 111 -j ACCEPT
-iptables -A INPUT -p tcp -s 210.61.2.239 --dport 111 -j ACCEPT
 # allow node from categories nfs
 iptables -A INPUT -p udp -s 210.65.11.197 --dport 111 -j ACCEPT
-iptables -A INPUT -p udp -s 210.61.2.239 --dport 111 -j ACCEPT
 # allow node from categories nfs
 iptables -A INPUT -p tcp -s 210.65.11.197 --dport 2049 -j ACCEPT
-iptables -A INPUT -p tcp -s 210.61.2.239 --dport 2049 -j ACCEPT
 # allow node from categories nfs
 iptables -A INPUT -p udp -s 210.65.11.197 --dport 2049 -j ACCEPT
-iptables -A INPUT -p udp -s 210.61.2.239 --dport 2049 -j ACCEPT
 # allow node from categories nfs
 iptables -A INPUT -p tcp -s 210.65.11.197 --dport 32764:32769 -j ACCEPT
-iptables -A INPUT -p tcp -s 210.61.2.239 --dport 32764:32769 -j ACCEPT
 # allow node from categories nfs
 iptables -A INPUT -p udp -s 210.65.11.197 --dport 32764:32769 -j ACCEPT
-iptables -A INPUT -p udp -s 210.61.2.239 --dport 32764:32769 -j ACCEPT
 sleep 30
 iptables -F
 iptables -X
--- firewall/outputs/210.65.11.197_test.sh
+++ firewall/outputs/210.65.11.197_test.sh
@@ -11,13 +11,11 @@ iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -A INPUT -p tcp -s 210.65.10.110 --dport 22 -j ACCEPT
 # allow loadbalancer, mainpage, node from categories mysql
 iptables -A INPUT -p tcp -s 210.65.10.110 --dport 3306 -j ACCEPT
-iptables -A INPUT -p tcp -s 210.61.2.239 --dport 3306 -j ACCEPT
 # allow all from categories pgsql
 iptables -A INPUT -p tcp --dport 5432 -j ACCEPT
 # allow loadbalancer from categories node
 iptables -A INPUT -p tcp -s 210.65.10.110 --dport 20001:29999 -j ACCEPT
 # allow node, mainpage from categories elastic_search
-iptables -A INPUT -p tcp -s 210.61.2.239 --dport 9200 -j ACCEPT
 iptables -A INPUT -p tcp -s 210.65.10.110 --dport 9200 -j ACCEPT
 sleep 30
 iptables -F
