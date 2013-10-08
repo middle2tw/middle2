43c104939fa5f24291655c09bae407b18a39fa9d
Ronny Wang <ronnywang@gmail.com>
2013-10-08 09:29:07 +0800

    將 210.65.11.197 也加入 node server

--- firewall/outputs/210.65.11.197_test.sh
+++ firewall/outputs/210.65.11.197_test.sh
@@ -7,13 +7,15 @@ iptables -P OUTPUT ACCEPT
 iptables -P FORWARD ACCEPT
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-# allow dev from categories ALL
+# allow dev, mainpage, loadbalancer from categories ALL, node
 iptables -A INPUT -p tcp -s 210.65.10.110 --dport 22 -j ACCEPT
 # allow loadbalancer, mainpage, node from categories mysql
 iptables -A INPUT -p tcp -s 210.65.10.110 --dport 3306 -j ACCEPT
 iptables -A INPUT -p tcp -s 210.61.2.239 --dport 3306 -j ACCEPT
 # allow all from categories pgsql
 iptables -A INPUT -p tcp --dport 5432 -j ACCEPT
+# allow loadbalancer from categories node
+iptables -A INPUT -p tcp -s 210.65.10.110 --dport 20001:29999 -j ACCEPT
 # allow node, mainpage from categories elastic_search
 iptables -A INPUT -p tcp -s 210.61.2.239 --dport 9200 -j ACCEPT
 iptables -A INPUT -p tcp -s 210.65.10.110 --dport 9200 -j ACCEPT
