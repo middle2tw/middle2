09bcba8033f40c28a419816f274b52d07efef2f1
Ronny Wang <ronnywang@gmail.com>
2015-02-26 13:52:16 +0800

    加上 SSLKey->get($key)

--- webdata/models/SSLKey.php
+++ webdata/models/SSLKey.php
@@ -1,11 +1,20 @@
 <?php
 
+class SSLKeyRow extends Pix_Table_Row
+{
+    public function get($k)
+    {
+        return json_decode($this->config)->{$k};
+    }
+}
+
 class SSLKey extends Pix_Table
 {
     public function init()
     {
         $this->_primary = 'domain';
         $this->_name = 'ssl_keys';
+        $this->_rowClass = 'SSLKeyRow';
 
         $this->_columns['domain'] = array('type' => 'char', 'size' => 64);
         $this->_columns['config'] = array('type' => 'text');
