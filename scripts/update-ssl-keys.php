b4a491b3eb88a3179b07e74799c0a4e2bbf04b5b
Ronny Wang <ronnywang@gmail.com>
2016-02-13 12:33:47 +0800

    add update ssl script

---
+++ scripts/update-ssl-keys.php
@@ -0,0 +1,44 @@
+<?php
+
+include(__DIR__ . '/../webdata/init.inc.php');
+
+if (is_dir($_SERVER['argv'][1])) {
+    chdir($_SERVER['argv'][1]);
+}
+
+$fp = fopen('domains.txt', 'r');
+$time = 0;
+while ($domain = fgets($fp)) {
+    if (!$domain = trim($domain)) {
+        continue;
+    }
+
+    if (!is_dir("certs/{$domain}")) {
+        throw new Exception("domain {$domain} not found");
+    }
+
+    if (preg_match('#cert-(.*).pem#', readlink("certs/{$domain}/cert.pem"), $matches)) {
+        $time = max($time, $matches[1]);
+    }
+
+    $config = new StdClass;
+    $config->ca = array(trim(file_get_contents("certs/{$domain}/fullchain.pem")));
+    $config->key = trim(file_get_contents("certs/{$domain}/privkey.pem"));
+    $config->cert = trim(file_get_contents("certs/{$domain}/cert.pem"));
+    if ($k = SSLKey::find($domain)) {
+        $k->update(array(
+            'config' => json_encode($config),
+        ));
+    } else {
+        SSLKey::insert(array(
+            'domain' => $domain,
+            'config' => json_encode($config),
+        ));
+    }
+}
+
+if ($time != trim(file_get_contents('/tmp/le_version'))) {
+    error_log('restart web server');
+    system("kill `cat /tmp/middle2.pid`");
+    file_put_contents('/tmp/le_version', $time);
+}
