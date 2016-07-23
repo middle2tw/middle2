316b1dbf97eb8139ef48e1ffc6d531ad50f9608c
Ronny Wang <ronnywang@gmail.com>
2016-07-23 10:04:27 +0800

    防火牆允許 icmp

--- firewall/gen.php
+++ firewall/gen.php
@@ -16,6 +16,8 @@ class FirewallGenerator
             'iptables -P FORWARD ACCEPT',
             'iptables -A INPUT -i lo -j ACCEPT',
             'iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT',
+            'iptables -A INPUT -p icmp --icmp-type 8 -s 0/0 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT',
+            'iptables -A OUTPUT -p icmp --icmp-type 0 -d 0/0 -m state --state ESTABLISHED,RELATED -j ACCEPT',
         );
     }
 
