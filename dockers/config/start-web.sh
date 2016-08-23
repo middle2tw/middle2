9a36218d41b09e5980051e4d90cc7d15929f1b4e
Ronny Wang <ronnywang@gmail.com>
2016-08-23 14:57:03 +0800

    加上給 docker 的 config

---
+++ dockers/config/start-web.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+export LOG_FILE=/srv/logs/web.log
+export PORT=80
+export HOME=/srv/web
+
+cd /srv/web
+
+if [ -f "/srv/web/Procfile" ]; then
+    `cat /srv/web/Procfile | grep '^web:' |  awk '{print substr($0, 5); }' | sed "s/\\$PORT/$PORT/"` > ${LOG_FILE} 2>&1 &
+elif [ -f "/srv/web/manage.py" ]; then
+ # python django
+    python ./manage.py runserver 0.0.0.0:80 --noreload > ${LOG_FILE} 2>&1 &
+elif [ -f "/srv/web/app.py" ]; then
+# python app.py
+    gunicorn app:app -b 0.0.0.0:80 > ${LOG_FILE} &
+elif [ -f "/srv/web/web.rb" ]; then
+# ruby
+    export RACK_ENV=production
+    ruby web.rb -p 80 > ${LOG_FILE} 2>&1 &
+#elif [ -f "/srv/web/index.php" ]; then
+else
+# build /srv/env.conf
+    env | awk -F '=' '{print "env[" $1 "]=\"" substr($0, index($0, "=") + 1) "\""}' > /srv/logs/env.conf
+
+    # start php-fpm
+    /usr/sbin/php5-fpm
+
+    # start apache
+    /usr/sbin/apache2ctl start
+fi
+
+START_AT=`date +%s`
+END_AT=`expr 300 + $START_AT`
+HTTP_CODE=0
+
+while [ $END_AT -gt `date +%s` ]
+do
+        HTTP_CODE=`curl --connect-timeout 3 --stderr /dev/null --include http://0:$PORT | head -n 1 | awk '{print $2}'`
+        echo $HTTP_CODE | grep '^[0-9]\+$' > /dev/null
+        if [ "$?" -eq "1" ]; then
+            sleep 1
+        else
+            if [ "$HTTP_CODE" = "" ]; then
+                sleep 1
+            elif [ $HTTP_CODE -eq 0 ]; then
+                sleep 1
+            else
+                END_AT=0
+            fi
+        fi
+done
+
+#TODO 失敗的話要讓 loadbalancer 知道, 可以從 END_AT 判斷
