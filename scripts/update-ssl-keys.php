ef33e3f7171c3864d9f360724170fbd9b48a5a06
Ronny Wang <ronnywang@gmail.com>
2018-06-12 18:02:05 +0800

    add wildcard domain support

--- scripts/update-ssl-keys.php
+++ scripts/update-ssl-keys.php
@@ -43,6 +43,47 @@ while ($domain = fgets($fp)) {
     }
 }
 
+$fp = fopen('dns-domains.txt', 'r');
+$time = 0;
+while ($domains = fgets($fp)) {
+    $domains = explode(' ', trim($domains));
+    $first_domain = $domains[0];
+    foreach ($domains as $domain) {
+        if (!$domain = trim($domain)) {
+            continue;
+        }
+
+        if (!is_dir("certs/{$first_domain}")) {
+            throw new Exception("domain {$first_domain} not found");
+        }
+
+        if (preg_match('#cert-(.*).pem#', readlink("certs/{$first_domain}/cert.pem"), $matches)) {
+            $time = max($time, $matches[1]);
+        }
+
+        $cert_pattern = '/-----BEGIN CERTIFICATE-----[^-]+-----END CERTIFICATE-----/s';
+        $key_pattern = '/-----BEGIN RSA PRIVATE KEY-----[^-]+-----END RSA PRIVATE KEY-----/s';
+
+        $config = new StdClass;
+        preg_match_all($cert_pattern, trim(file_get_contents("certs/{$first_domain}/fullchain.pem")), $matches);
+        $config->ca = $matches[0];
+        preg_match($cert_pattern, trim(file_get_contents("certs/{$first_domain}/cert.pem")), $matches);
+        $config->cert = $matches[0];
+        preg_match($key_pattern, trim(file_get_contents("certs/{$first_domain}/privkey.pem")), $matches);
+        $config->key = $matches[0];
+        if ($k = SSLKey::find($domain)) {
+            $k->update(array(
+                'config' => json_encode($config),
+            ));
+        } else {
+            SSLKey::insert(array(
+                'domain' => $domain,
+                'config' => json_encode($config),
+            ));
+        }
+    }
+}
+
 if ($time != trim(file_get_contents('/tmp/le_version'))) {
     error_log('restart web server');
     system("systemctl reload m2-lb.service");
