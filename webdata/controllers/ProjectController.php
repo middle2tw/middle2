5aa71ebce7caa5ddfd481c1336ea9bc264c0adca
Ronny Wang <ronnywang@gmail.com>
2020-03-23 23:11:30 +0800

    admin can see cronlog

--- webdata/controllers/ProjectController.php
+++ webdata/controllers/ProjectController.php
@@ -433,7 +433,7 @@ class ProjectController extends Pix_Controller
         }
         $this->view->project = $project;
 
-        if (!$project->isMember($this->user)) {
+        if (!$project->isMember($this->user) and !$this->user->isAdmin()) {
             return $this->alert('Project not found', '/');
         }
     }
